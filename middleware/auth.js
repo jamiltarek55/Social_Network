c193238a19660bd1cb787b524565bbda548e87ef
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-06-08 17:35:00 +0600

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization");
-
     if (!token) return res.status(400).json({ msg: "Invalid Authentication." });
 
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
